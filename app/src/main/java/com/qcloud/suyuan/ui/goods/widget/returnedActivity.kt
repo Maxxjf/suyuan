e84ef60f513ed72dd34eaaee13a7180e027757df
iceberg <841100358@qq.com>
2018-03-20 18:07:05 +0800

    退货xml布局

--- app/src/main/java/com/qcloud/suyuan/ui/goods/widget/returnedActivity.kt
+++ app/src/main/java/com/qcloud/suyuan/ui/goods/widget/returnedActivity.kt
@@ -1,23 +1,31 @@
 package com.qcloud.suyuan.ui.goods.widget
 
+import android.content.Context
+import android.content.Intent
 import com.qcloud.suyuan.R
 import com.qcloud.suyuan.base.SwipeBaseActivity
 import com.qcloud.suyuan.ui.goods.presenter.impl.IReturnedPersenterImpl
 import com.qcloud.suyuan.ui.goods.view.IReturnedView
 
 /**
- * 类型：returnedActivity
+ * 类型：ReturnedActivity
  * Author: iceberg
  * Date: 2018/3/19.
  * 退货
  */
-class returnedActivity :SwipeBaseActivity<IReturnedView,IReturnedPersenterImpl>(),IReturnedView{
+class ReturnedActivity :SwipeBaseActivity<IReturnedView,IReturnedPersenterImpl>(),IReturnedView{
     override val layoutId: Int
-        get() = R.layout.activity_cart
+        get() = R.layout.activity_returned
 
     override fun initPresenter(): IReturnedPersenterImpl? = IReturnedPersenterImpl()
 
     override fun initViewAndData() {
 
+    }
+    companion object {
+        fun openActivity(context: Context){
+            val intent=Intent(context,ReturnedActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/qcloud/suyuan/ui/order/view/IReturnView.kt
+++
@@ -1,9 +0,0 @@
-package com.qcloud.suyuan.ui.order.view
-
-/**
- * Description: 退货
- * Author: gaobaiqiang
- * 2018/3/20 上午8:48.
- */
-interface IReturnView {
-}
