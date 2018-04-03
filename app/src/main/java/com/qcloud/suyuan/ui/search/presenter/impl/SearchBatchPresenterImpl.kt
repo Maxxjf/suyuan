890700047551881c41123ae060fc5f11f84dea48
kuzan <kuzan@qi-cloud.com>
2018-04-03 17:18:02 +0800

    修改url常量类

--- app/src/main/java/com/qcloud/suyuan/ui/search/presenter/impl/SearchBatchPresenterImpl.kt
+++ app/src/main/java/com/qcloud/suyuan/ui/search/presenter/impl/SearchBatchPresenterImpl.kt
@@ -1,6 +1,11 @@
 package com.qcloud.suyuan.ui.search.presenter.impl
 
 import com.qcloud.qclib.base.BasePresenter
+import com.qcloud.qclib.beans.ReturnDataBean
+import com.qcloud.qclib.callback.DataCallback
+import com.qcloud.suyuan.beans.ProductBean
+import com.qcloud.suyuan.model.IStorageModel
+import com.qcloud.suyuan.model.impl.StorageModelImpl
 import com.qcloud.suyuan.ui.search.presenter.ISearchBatchPresenter
 import com.qcloud.suyuan.ui.search.view.ISearchBatchView
 
@@ -10,4 +15,28 @@ import com.qcloud.suyuan.ui.search.view.ISearchBatchView
  * 2018/4/1 下午11:03.
  */
 class SearchBatchPresenterImpl: BasePresenter<ISearchBatchView>(), ISearchBatchPresenter {
+
+    private val mModel: IStorageModel = StorageModelImpl()
+
+    /**
+     * 获取产品
+     *
+     * @param keyword 产品批次码
+     * */
+    override fun loadProduct(keyword: String) {
+        mModel.searchList(keyword, object : DataCallback<ReturnDataBean<ProductBean>> {
+            override fun onSuccess(t: ReturnDataBean<ProductBean>?, message: String?) {
+                if (t?.list != null && t.list!!.isNotEmpty()) {
+                    mView?.replaceData(t.list!![0])
+                } else {
+                    mView?.loadErr("暂无数据")
+                }
+            }
+
+            override fun onError(status: Int, message: String) {
+                mView?.loadErr(message, true)
+                mView?.showEmptyView(message)
+            }
+        })
+    }
 }
